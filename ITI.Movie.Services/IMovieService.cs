939433454c951c60e6b1cfc019521888dca6153b
Khalid Mosaad <kmosaad26@gmail.com>
2021-06-10 18:41:02 +0200

    Add project files.

---
+++ ITI.Movie.Services/IMovieService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Movie.Services
+{
+    [ServiceContract]
+    public interface IMovieService
+    {
+        [OperationContract]
+        List<Movie> GetAll();
+        [OperationContract]
+        Movie Add(Movie movie);
+        [OperationContract]
+        Movie Modify(Movie movie);
+
+        [OperationContract]
+        void Remove(int id);
+    }
+}
---
+++ ITI.Movie.Services/MovieService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ITI.Movie.Services
+{
+    public class MovieService: IMovieService
+    {
+        private static List<Movie> movies = new List<Movie> { 
+            new Movie{Id =1,Name="Toy Store"},
+            new Movie{Id=2,Name="Lion King"}
+        };
+        public Movie Add(Movie movie)
+        {
+            movies.Add(movie);
+            return movie;
+        }
+        public List<Movie> GetAll()
+        {
+            return movies;
+        }
+        public Movie Modify(Movie movie)
+        {
+            movies.RemoveAll(m => m.Id == movie.Id);
+            movies.Add(movie);
+            return movie;
+        }
+        public void Remove(int id)
+        {
+            movies.RemoveAll(m=>m.Id==id);
+        }
+    }
+}
